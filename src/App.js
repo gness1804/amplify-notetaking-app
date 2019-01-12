b3f50e4081b3ff8c1996163706c50c0933d0ed27
gness1804 <graham.nessler@gmail.com>
2019-01-12 15:13:14 -0600

    Set up authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        App
-      </div>
-    );
+    return <div>I should only be seen if the user is signed in.</div>;
   }
 }
 
-export default App;
+export default withAuthenticator(App, {
+  includeGreetings: true,
+});
