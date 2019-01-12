b4c710ec51f142ad35196ae8f33ae76baec9aa34
gness1804 <graham.nessler@gmail.com>
2019-01-12 14:32:11 -0600

    Initial commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,24 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createNote = `mutation CreateNote($input: CreateNoteInput!) {
+  createNote(input: $input) {
+    id
+    details
+  }
+}
+`;
+export const updateNote = `mutation UpdateNote($input: UpdateNoteInput!) {
+  updateNote(input: $input) {
+    id
+    details
+  }
+}
+`;
+export const deleteNote = `mutation DeleteNote($input: DeleteNoteInput!) {
+  deleteNote(input: $input) {
+    id
+    details
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,24 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    details
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    details
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    details
+  }
+}
+`;
