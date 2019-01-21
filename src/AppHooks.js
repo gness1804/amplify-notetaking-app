49db9f560fdbb350a66538c6ebf401c0b6c86984
gness1804 <graham.nessler@gmail.com>
2019-01-20 18:45:22 -0600

    Finish refactor but have not yet tested

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -33,11 +33,6 @@ const App = () => {
           ...notes.slice(targetIndex + 1),
         ];
 
-        // this.setState({
-        //   notes: newNotes,
-        //   details: '',
-        //   id: '',
-        // });
         setNotes(newNotes);
         setDetails('');
         setId('');
@@ -51,10 +46,8 @@ const App = () => {
     ).subscribe({
       next: noteData => {
         const { id: deletedNoteId } = noteData.value.data.onDeleteNote;
-        const newNotes = this.state.notes.filter(n => n.id !== deletedNoteId);
-        this.setState({
-          notes: newNotes,
-        });
+        const newNotes = notes.filter(n => n.id !== deletedNoteId);
+        setNotes(newNotes);
       },
     });
   };
@@ -65,25 +58,19 @@ const App = () => {
     ).subscribe({
       next: noteData => {
         const newNote = noteData.value.data.onCreateNote;
-        const prevNotes = this.state.notes.filter(n => n.id !== newNote.id);
-        this.setState({
-          notes: [...prevNotes, newNote],
-        });
+        const prevNotes = notes.filter(n => n.id !== newNote.id);
+        setNotes([...prevNotes, newNote]);
       },
     });
   };
 
   const getAllNotes = async () => {
     const initResult = await API.graphql(graphqlOperation(listNotes));
-    const notes = initResult.data.listNotes.items;
-    this.setState({
-      notes,
-    });
+    setNotes(initResult.data.listNotes.items);
   };
 
   useEffect(async () => {
     await getAllNotes();
-    // where the old state was frozen on component mount
     subscribeToCreateNote();
     subscribeToDeleteNote();
     subscribeToUpdateNote();
@@ -96,23 +83,20 @@ const App = () => {
   }, []);
 
   const hasExistingNote = () => {
-    const { notes, id } = this.state;
     if (id) {
       return notes.filter(n => n.id === id).length > 0;
     }
     return false;
   };
 
-  const handleSetNote = ({ id, details }) => {
-    this.setState({
-      id,
-      details,
-    });
+  const handleSetNote = ({ noteId, noteDetails }) => {
+    setId(noteId);
+    setDetails(noteDetails);
   };
 
-  const handleDeleteNote = async id => {
+  const handleDeleteNote = async noteId => {
     const input = {
-      id,
+      id: noteId,
     };
     await API.graphql(
       graphqlOperation(deleteNote, {
@@ -122,32 +106,13 @@ const App = () => {
   };
 
   const handleChangeNote = e => {
-    this.setState({
-      details: e.target.value,
-    });
+    setDetails(e.target.value);
   };
 
-  const handleAddNote = async e => {
-    const { details, id } = this.state;
-    e.preventDefault();
-    if (!details) {
-      alert('Oops! Your note must include text.');
-      return;
-    }
-    // check if we have an existing note. If so, update it.
-    if (this.hasExistingNote()) {
-      // we have an existing note. Update it in the db.
-      await this.updateNote(id, details);
-    } else {
-      // create a new note.
-      await this.createNoteFromScratch(details);
-    }
-  };
-
-  const updateNote = async (id, details) => {
+  const updateNoteFunc = async (noteId, noteDetails) => {
     const input = {
-      id,
-      details,
+      id: noteId,
+      details: noteDetails,
     };
     await API.graphql(
       graphqlOperation(updateNote, {
@@ -156,39 +121,49 @@ const App = () => {
     );
   };
 
-  const createNoteFromScratch = async details => {
+  const createNoteFromScratch = async noteDetails => {
     const input = {
-      details,
+      details: noteDetails,
     };
     await API.graphql(graphqlOperation(createNote, { input }));
-    this.setState({
-      details: '',
-    });
+    setDetails('');
+  };
+
+  const handleAddNote = async e => {
+    e.preventDefault();
+    if (!details) {
+      alert('Oops! Your note must include text.');
+      return;
+    }
+    // check if we have an existing note. If so, update it.
+    if (hasExistingNote()) {
+      // we have an existing note. Update it in the db.
+      await updateNoteFunc(id, details);
+    } else {
+      // create a new note.
+      await createNoteFromScratch(details);
+    }
   };
 
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-1">Notetaker</h1>
-      <form onSubmit={this.handleAddNote} className="mb3">
+      <form onSubmit={handleAddNote} className="mb3">
         <input
           type="text"
           className="pa2 f4"
           placeholder="Write your note."
-          onChange={this.handleChangeNote}
+          onChange={handleChangeNote}
           value={details}
         />
         <button className="pa2 f4" type="submit">
-          {this.hasExistingNote() ? (
-            <span>Update Note</span>
-          ) : (
-            <span>Add Note</span>
-          )}
+          {hasExistingNote() ? <span>Update Note</span> : <span>Add Note</span>}
         </button>
       </form>
       <NotesList
         notes={notes}
-        handleDeleteNote={this.handleDeleteNote}
-        handleSetNote={this.handleSetNote}
+        handleDeleteNote={handleDeleteNote}
+        handleSetNote={handleSetNote}
       />
     </div>
   );
