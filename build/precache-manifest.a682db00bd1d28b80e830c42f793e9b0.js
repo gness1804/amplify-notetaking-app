f18fee3b12286f97d21bc57fbe90d18e1c289422
gness1804 <graham.nessler@gmail.com>
2019-01-24 19:49:33 -0600

    Add build dir

---
+++ build/precache-manifest.a682db00bd1d28b80e830c42f793e9b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "516610d79ec1ae80dcef",
+    "url": "/static/js/main.516610d7.chunk.js"
+  },
+  {
+    "revision": "1231ebd4dbbca3f2a3b6",
+    "url": "/static/js/1.1231ebd4.chunk.js"
+  },
+  {
+    "revision": "1231ebd4dbbca3f2a3b6",
+    "url": "/static/css/1.2204c0e1.chunk.css"
+  },
+  {
+    "revision": "2b0b7c30dfe3c8622d2f4513f7cfc5cd",
+    "url": "/index.html"
+  }
+];
